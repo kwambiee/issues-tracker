96af9a6f24089314ef36d6fbfab44c5f40d7d7fd
Joy Kwamboka <kwambokaj2.jk@gmail.com>
2025-05-27 13:33:43 +0300

    Add migrations for projects and issues, and update schema version

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,17 +1,17 @@
 class Issue < ApplicationRecord
-  belongs_to :project
+    belongs_to :project
 
-  enum status: {
-    new: "New",
-    in_progress: "In Progress",
-    closed: "Closed"
-  }
+    enum :status, { 
+  new: "New",
+  in_progress: "In Progress", 
+  closed: "Closed" 
+}, prefix: true, default: :new
 
-  enum priority: {
-    low: 1,
-    medium: 3,
-    high: 5
-  }
-
-  validates :title, :status, :priority, presence: true
-end
+enum :priority, { 
+  low: 1, 
+  medium: 3, 
+  high: 5 
+}, prefix: true  
+  
+    validates :title, :status, :priority, presence: true
+  end
